b044cb1843e6318da5fe8bd41cdf95bffb1b6a80
DESKTOP-ADP <s.ward666@live.ie>
2018-05-19 13:13:07 +0100

    Blue Fire spawns for Items added
    Blue Fire spawns added from following Items
    
    Hot Bombs
    Ghost Pepper
    Red Candle

--- main.lua
+++ main.lua
@@ -6,6 +6,11 @@ local game = Game()
 
 -- Trinket
 TrinketType.TRINKET_ICYHOT = Isaac.GetTrinketIdByName("Icy Hot")
+EntityType.BLUE_FLAME_ADP = Isaac.GetEntityTypeByName("Blue Flame")
+
+-- Item Data Tables
+local IcyHot = { ACTIVATION_CHANCE = 0.4 }
+local test
 
 -- Functions
 
@@ -26,15 +31,47 @@ end
 function IcyHotADP:OnRoomLoad(entity, variant, subtype, gridIndex, seed)
 	local player = Isaac.GetPlayer(0);
 
-	if player:HasTrinket(TrinketType.TRINKET_ICYHOT) then
+	if player:HasTrinket(TrinketType.TRINKET_ICYHOT) and math.random() <= IcyHot.ACTIVATION_CHANCE then
 		if entity == EntityType.ENTITY_FIREPLACE and variant ~= 2 then
 			return {EntityType.ENTITY_FIREPLACE, 2, 0}
 		end
 	end
+end
 
+function IcyHotADP:OnEffectSpawn(effect)
+	local player = Isaac.GetPlayer(0);
+
+	if player:HasTrinket(TrinketType.TRINKET_ICYHOT) and math.random() <= IcyHot.ACTIVATION_CHANCE then
+		if effect.Variant == EffectVariant.RED_CANDLE_FLAME then
+			effect:GetSprite():ReplaceSpritesheet(0, "resources/gfx/effects/effect_034_bluefire.png")
+			effect:GetSprite():LoadGraphics()
+			effect.State = -3
+		elseif effect.Variant == EffectVariant.HOT_BOMB_FIRE then
+			effect:GetSprite():ReplaceSpritesheet(0, "resources/gfx/effects/effect_034_bluefire.png")
+			effect:GetSprite():LoadGraphics()
+			effect.State = -1
+		end
+	end
 
 end
 
+function IcyHotADP:OnEffectUpdate(effect)
+	if effect.Variant == EffectVariant.HOT_BOMB_FIRE then
+			if effect.State == -1 then
+				effect:SetTimeout(500)
+				effect.State = 0
+			end
+			test = effect.Timeout
+	end
+end
+
+function IcyHotADP:Update()
+	Isaac.RenderText(tostring(test) .. " " .. tostring(EffectVariant.HOT_BOMB_FIRE), 40, 40, 255, 255, 255, 255)
+end
+
 -- Callbacks
+IcyHotADP:AddCallback(ModCallbacks.MC_POST_UPDATE, IcyHotADP.Update)
 IcyHotADP:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, IcyHotADP.OnGameStart)
 IcyHotADP:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, IcyHotADP.OnRoomLoad)
+IcyHotADP:AddCallback(ModCallbacks.MC_POST_EFFECT_INIT, IcyHotADP.OnEffectSpawn) 
+IcyHotADP:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, IcyHotADP.OnEffectUpdate)   
